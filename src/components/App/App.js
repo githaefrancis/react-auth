3003d82912396da5a23ba1d1a56d6384ca9ab3b3
Francis Githae <mureithigithae@gmail.com>
2022-06-03 15:36:28 +0300

    add custom hook to detect login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,10 +4,24 @@ import {BrowserRouter,Route,Routes} from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
+import useToken from './useToken';
 
+
+// function setToken(userToken){
+//   sessionStorage.setItem('token',JSON.stringify(userToken));
+// }
+
+// function getToken(){
+// const tokenString=sessionStorage.getItem('token');
+// const userToken=JSON.parse(tokenString);
+// return userToken?.token
+// }
 
 function App() {
-  const [token,setToken]=useState();
+  // const [token,setToken]=useState();
+  // const token=getToken()
+  const {token,setToken}=useToken();
+
   if(!token){
     return <Login setToken={setToken} />
   }
